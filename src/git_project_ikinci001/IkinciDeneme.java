a9bc188a8dec1bcd6b8cbe22507bc0c1957967f3
Salihonder2020 <you@example.com>
2020-08-09 23:22:51 +0200

    Gecenin bu saatinde git github calisiyorum

---
+++ src/git_project_ikinci001/IkinciDeneme.java
@@ -0,0 +1,14 @@
+package git_project_ikinci001;
+
+public class IkinciDeneme {
+
+	public static void main(String[] args) {
+		System.out.println("Ikinci Deneme git projemm");
+		System.out.println("Ikinci Deneme github da degistirdim");
+		System.out.println("Salih bu is Allahin izniyle olacak");
+		System.out.println("Ikinci Deneme Cakisma");
+		System.out.println("Ikinci Deneme conflict");
+
+	}
+
+}
---
+++ src/git_project_ikinci001/IkinciDenemeeeee.java
@@ -0,0 +1,14 @@
+package git_project_ikinci001;
+
+public class IkinciDenemeeeee {
+
+	public static void main(String[] args) {
+		System.out.println("Ikinci Denemeeeee git projemm");
+		System.out.println("Ikinci Denemeeeee github da degistirdim");
+		System.out.println("Salih bu is Allahin izniyle olacak");
+		System.out.println("Ikinci Denemeeee Cakisma");
+		System.out.println("Ikinci Denemeeee conflict");
+
+	}
+
+}
